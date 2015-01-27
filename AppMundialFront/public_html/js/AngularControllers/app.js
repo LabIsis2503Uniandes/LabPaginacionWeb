ce79f709c4e7e05b502a9e41cfa575ddbd59f12a
Jhonatan <jhonatan.alarcon.ariza@gmail.com>
2015-01-27 11:04:06 -0500

    Codigo Inicial FrontEnd
    Se realiza el commit de la aplicacion Inicial

---
+++ AppMundialFront/public_html/js/AngularControllers/app.js
@@ -0,0 +1,55 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+(function () {
+    var aplicacionMundial = angular.module('aplicacionMundial', []);
+
+
+
+    aplicacionMundial.directive('toolbar', function () {
+        return{
+            restrict: 'E',
+            templateUrl: 'toolbar.html',
+            controller: function () {
+                this.tab = 0;
+                this.selectTab = function (setTab) {
+                    this.tab = setTab;
+                };
+                this.isSelected = function (tabParam) {
+                    return this.tab === tabParam;
+                };
+            },
+            controllerAs: 'toolbar'
+        };
+    });
+
+
+
+
+    aplicacionMundial.directive('competitorInfo', function () {
+        return{
+            restrict: 'E',
+            templateUrl: 'competitorInfo.html',
+            controller: ['$http', '$scope', function ($http, $scope) {
+                    var self = this;
+                    $scope.api = function () {
+                        var callApi = $http.get('http://localhost:8083/webresources/competitors').success(function (data) {
+                            self.competitors = data.competitors;
+                            $scope.total = data.totalRecords;
+                        });
+                    }
+                    $scope.api();
+                }],
+            controllerAs: 'CompetitorsListCtrl'
+        };
+    });
+
+
+
+
+})();
+
